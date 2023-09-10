42e184b217a3207a12f566229916b34283474788
Shaikh Haris Ali <57299748+ShaikhHarisAli@users.noreply.github.com>
2023-09-11 01:40:14 +0500

    Ant Design Icon
    Ant Design Icon

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,21 @@
 import React from "react";
-import {Switch,Route,Router} from 'react-router-dom'
+import {Switch,Route} from 'react-router-dom'
 import Login from '../src/pages/auth/Login'
 import Register from '../src/pages/auth/Register'
 import Home from '../src/pages/Home'
+import Header from "./component/nav/Header";
+
 
 const App = () => {
   return (
-    
+    <>
+    <Header/>
     <Switch>
       <Route exact path ="/" component={Home}/>
       <Route exact path ="/login" component={Login}/>
       <Route exact path ="/register" component={Register}/>
     </Switch>
+    </>
    
   )
 }
