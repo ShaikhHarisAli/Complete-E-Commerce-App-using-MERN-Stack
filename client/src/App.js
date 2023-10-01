df35b25416a9d22d044393b496f6d0a7a432aa51
Shaikh Haris Ali <57299748+ShaikhHarisAli@users.noreply.github.com>
2023-10-01 20:13:48 +0500

    Complete Registration Page

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import Login from '../src/pages/auth/Login'
 import Register from '../src/pages/auth/Register'
 import Home from '../src/pages/Home'
 import Header from "./component/nav/Header";
+import RegisterComplete from "./pages/auth/RegisterComplete";
 
 
 const App = () => {
@@ -17,6 +18,7 @@ const App = () => {
       <Route exact path ="/" component={Home}/>
       <Route exact path ="/login" component={Login}/>
       <Route exact path ="/register" component={Register}/>
+      <Route exact path="/register/complete" component={RegisterComplete}/>
     </Switch>
     </>
    
