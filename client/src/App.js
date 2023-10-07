a4644ffbceb0902b77ef3fbcead2340407d1be11
Shaikh Haris Ali <57299748+ShaikhHarisAli@users.noreply.github.com>
2023-10-07 15:38:11 +0500

    User in Redux State

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect} from "react";
 import {Switch,Route} from 'react-router-dom'
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
@@ -7,9 +7,36 @@ import Register from '../src/pages/auth/Register'
 import Home from '../src/pages/Home'
 import Header from "./component/nav/Header";
 import RegisterComplete from "./pages/auth/RegisterComplete";
+import {auth} from './firebase'
+import { useDispatch } from "react-redux";
 
 
 const App = () => {
+  const dispatch = useDispatch()
+
+  // to check fiebase auth state
+  useEffect(()=>{
+    const unsubscribe = auth.onAuthStateChanged(async (user) => {
+      if (user) {
+        const idTokenResult = await user.getIdTokenResult();
+        // console.log("user", user);
+        // console.log("idTokenResult", idTokenResult);
+        dispatch({
+          type: "LOGGED_IN_USER",
+          payload: {
+            email: user.email,
+            token: idTokenResult.token,
+          },
+        });
+      }
+    });
+    // cleanup
+    return () => unsubscribe();
+  }, []);
+
+  
+
+
   return (
     <>
     <Header/>
